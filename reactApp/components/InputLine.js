5ee3ece75aedcf0bb9c42669647f15350873c9e0
Owen Hines <owenhines07@gmail.com>
2017-07-19 15:50:32 -0700

    Added 'add todo' functionality with custom input

--- reactApp/components/InputLine.js
+++ reactApp/components/InputLine.js
@@ -3,15 +3,37 @@ import React from 'react';
 class InputLine extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      typedText: ''
+    }
+  }
+
+  handleTyping(e) {
+    this.setState({
+      typedText: e.target.value
+    })
+  }
+
+  handleSubmit() {
+    this.props.submit(this.state.typedText)
+    this.setState({
+      typedText: ''
+    })
+  }
+
+  handleFormSubmit(e) {
+    e.preventDefault()
+    this.handleSubmit()
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={(e) => this.handleFormSubmit(e)}>
         <div className="form-group">
           <div className="input-group">
-            <input type="text" className="form-control"></input>
-            <div type="submit" className="btn btn-primary input-group-addon" onClick={() => this.props.submit('Test Task')}>Add todo</div>
+            <input type="text" className="form-control" value={this.state.typedText} onChange={(e) => this.handleTyping(e)}></input>
+            <div type="submit" className="btn btn-primary input-group-addon" onClick={() => this.handleSubmit()}>Add todo</div>
           </div>
         </div>
       </form>
