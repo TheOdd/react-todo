11a77efebabe9c9b7f37387badaf853ee518ec2c
Owen Hines <owenhines07@gmail.com>
2017-07-19 14:41:12 -0700

    Added static page for dummy list of tasks with X buttons

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,5 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<p>React lives!</p>,
+const dummyData = [
+  'Clean the dishes',
+  'Mow the lawn',
+  'Get groceries',
+  'Get birthday present for mom'
+]
+
+class Todo extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <li>
+        <button>X</button>
+        {this.props.task}
+      </li>
+    )
+  }
+}
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <ul>
+        {dummyData.map(task => {
+          return <Todo task={task} />
+        })}
+      </ul>
+    )
+  }
+}
+
+ReactDOM.render(<TodoList />,
    document.getElementById('root'));
